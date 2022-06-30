5fc2d969a91c1d10dde45552215e4eb8b53b8f2c
jaiswalpkj <jaiswalpankaj884@gmail.com>
2022-06-30 19:39:39 +0530

    second change

--- src/app/services/user.service.ts
+++ src/app/services/user.service.ts
@@ -2,50 +2,58 @@ import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import Flight from '../Entity/Flight';
 
-const BASE_URL="http://ec2-65-0-30-232.ap-south-1.compute.amazonaws.com:8086/user";
+const BASE_URL =
+  'http://ec2-65-0-30-232.ap-south-1.compute.amazonaws.com:8086/user/';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class UserService {
+  flights: Flight[];
 
-  flights:Flight[];
+  searchFlight(search) {
+    return this.http.get(
+      BASE_URL +
+        '/searchFlight/' +
+        search.date +
+        '/' +
+        search.from +
+        '/' +
+        search.to
+    );
+  }
 
-searchFlight(search){
-  return this.http.get(BASE_URL+'/searchFlight/'+search.date+'/'+search.from+'/'+search.to)
-}
+  getFlight(id) {
+    return this.http.get(BASE_URL + '/flight/' + id);
+  }
 
-getFlight(id){
-  return this.http.get(BASE_URL+'/flight/'+id)
-}
+  bookFlight(reservation: any, id) {
+    return this.http.post(BASE_URL + '/bookflight/' + id, reservation);
+  }
 
-bookFlight(reservation:any,id){
-  return this.http.post(BASE_URL+'/bookflight/'+id,reservation)
-}
+  getLastTicket() {
+    return this.http.get(BASE_URL + '/last');
+  }
 
-getLastTicket(){
-  return this.http.get(BASE_URL+'/last')
-}
+  findTicketByPnr(pnr) {
+    return this.http.get(BASE_URL + '/ticket/' + pnr);
+  }
 
-findTicketByPnr(pnr){
-  return this.http.get(BASE_URL+'/ticket/'+pnr)
-}
+  findTicketByEmail(email) {
+    return this.http.get(BASE_URL + '/booking/history/' + email);
+  }
 
-findTicketByEmail(email){
-  return this.http.get(BASE_URL+'/booking/history/'+email)
-}
+  deleteTicket(reservation: any) {
+    return this.http.delete(BASE_URL + '/cancel/' + reservation.id);
+  }
 
-deleteTicket(reservation:any){
- return this.http.delete(BASE_URL+'/cancel/'+reservation.id)
-}
+  // setFlightDetails(data){
+  //   this.flights=data;
+  // }
 
-// setFlightDetails(data){
-//   this.flights=data;
-// }
+  // getFlightDetails(){
+  //   return this.flights;
+  // }
 
-// getFlightDetails(){
-//   return this.flights;
-// }
-
-  constructor(private http:HttpClient) { }
+  constructor(private http: HttpClient) {}
 }
